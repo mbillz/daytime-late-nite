9e9796051c70486b64017df082dda68234ec90a3
Matt Billings <matt.billings@calm.com>
2022-04-16 12:21:52 -0700

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,46 @@
+<main>
+	<div class="bg" />
+	<h1>daytime late nite is a feeling.</h1>
+</main>
+
+<style>
+	main {
+		align-items: center;
+		height: 100vh;
+		display: flex;
+		justify-content: center;
+		text-align: center;
+	}
+
+	@keyframes bgColor {
+		0% {
+			background-color: #048a81;
+		}
+		25% {
+			background-color: #06d6a0;
+		}
+		50% {
+			background-color: #54c6eb;
+		}
+		75% {
+			background-color: #8a89c0;
+		}
+		100% {
+			background-color: #cda2ab;
+		}
+	}
+
+	.bg {
+		animation: bgColor 20000ms infinite alternate;
+		bottom: 0;
+		left: 0;
+		position: absolute;
+		right: 0;
+		top: 0;
+		z-index: -1;
+	}
+
+	h1 {
+		font-size: 2.5rem;
+	}
+</style>
